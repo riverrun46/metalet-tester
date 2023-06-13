cd7061fd2dff7fcd54f1100a5111ad0dc8de619b
Vincent Tse <river_run@126.com>
2023-06-13 18:21:02 +0800

    add broadcast

---
+++ app/api/broadcast/route.ts
@@ -0,0 +1,27 @@
+export async function POST(request: Request) {
+  const requestJson = await request.json()
+  const rawTx = requestJson.rawTx
+  const network = requestJson.network || 'testnet'
+
+  if (!rawTx) {
+    return new Response('rawTx is required', { status: 400 })
+  }
+
+  const url = network === 'testnet' ? `https://mempool.space/testnet/api/tx` : `https://mempool.space/api/tx`
+  const res: any = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: rawTx,
+  }).then((res) => res.json())
+
+  return new Response(JSON.stringify(res), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+    status: 200,
+  })
+}
