ce7f37b41bdb0ab1e1ee60b9fc27d602b8330088
Vincent Tse <river_run@126.com>
2024-04-17 19:49:09 +0800

    feat: add more proxy api

---
+++ app/api/runes/balance/route.ts
@@ -0,0 +1,32 @@
+export const dynamic = 'force-dynamic'
+
+import { DOMAIN } from '@/app/data/constants'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const address = searchParams.get('address')
+
+  if (!address) {
+    return new Response('address is required', { status: 400 })
+  }
+
+  const url = `${DOMAIN}/address/${address}/runes/balance-list?start=0&limit=50`
+  const res: any = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Client': 'UniSat Wallet',
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+      Authorization: 'Bearer 9e73bfef04b578ac0d7a95f3bdde48c409a68486e32ceb7afb756657a12fb3a5',
+    },
+  }).then((res) => res.json())
+
+  return new Response(JSON.stringify(res), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+    status: 200,
+  })
+}
---
+++ app/data/constants.ts
@@ -0,0 +1,3 @@
+export const DOMAIN = (process.env.NETWORK = 'testnet'
+  ? 'https://open-api-testnet.unisat.io/v1/indexer'
+  : 'https://open-api.unisat.io/v1/indexer')
